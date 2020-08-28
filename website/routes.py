f1c0336f85d34558da6c7d0c852887f8df5bfd76
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-08-28 16:03:20 -0400

    cleaned up site and started text additions

---
+++ website/routes.py
@@ -0,0 +1,9 @@
+from website import app
+from flask import render_template
+
+
+# make a home page
+@app.route('/')
+@app.route('/home', methods=['GET', 'POST'])
+def home():
+    return render_template('index.html')
