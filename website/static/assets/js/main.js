91a386fe2ca31bb6104744ac0701bd5cb277d79b
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-09-16 15:02:47 -0400

    removed nav transition

--- website/static/assets/js/main.js
+++ website/static/assets/js/main.js
@@ -117,15 +117,6 @@
     });
   });
 
-  // Toggle .header-scrolled class to #header when page is scrolled
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
-    }
-  });
-
   if ($(window).scrollTop() > 100) {
     $('#header').addClass('header-scrolled');
   }
