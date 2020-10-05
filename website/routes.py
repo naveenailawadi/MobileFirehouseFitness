f92f20c2f24b389f67ddd7e071498c834b11b73f
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2020-10-05 11:27:27 -0400

    removed gallery highlight tags and made iterable

--- website/routes.py
+++ website/routes.py
@@ -1,5 +1,6 @@
 from website import app
 from flask import render_template
+import os
 
 
 # make a home page
@@ -18,4 +19,15 @@ def covid():
 # add a gallery page
 @app.route('/gallery')
 def gallery():
-    return render_template('gallery.html', title='Gallery')
+    # get the gallery folder
+    gallery_folder = f"{app.static_folder}/assets/img/gallery"
+
+    # get all the files here
+    files = os.listdir(gallery_folder)
+
+    photos = [
+        f"assets/img/gallery/{file}" for file in files if '.jpg' in file]
+
+    photos.sort()
+
+    return render_template('gallery.html', title='Gallery', photos=photos)
